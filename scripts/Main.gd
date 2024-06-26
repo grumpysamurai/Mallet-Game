b4d4cb0c867ca808c8d75b3d4b418b50f875c993
grumpysamurai <142862932+grumpysamurai@users.noreply.github.com>
2024-06-26 14:27:40 -0600

    Added targets into the game

--- scripts/Main.gd
+++ scripts/Main.gd
@@ -1,6 +1,10 @@
 extends Node2D
 var mouseposition
 @onready var cursor = $Cursor
+var ontarget = false
+var score = 0
+var iscooldownactive = false
+@onready var scoretext = %Score
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -14,3 +18,18 @@ func _process(delta):
 	
 	if Input.is_action_just_pressed("ui_cancel"):
 		get_tree().quit()
+	if Input.is_action_just_pressed("leftclick") && ontarget == true:
+		if iscooldownactive == false:
+			score += 1
+			print(score)
+			scoretext.text = str(score)
+			iscooldownactive = true
+		
+
+func _on_area_2d_area_entered(area):
+	ontarget = true
+
+
+func _on_area_2d_area_exited(area):
+	ontarget = false
+	iscooldownactive = false
